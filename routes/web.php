30f6f6d77ab18b94cfa9598b61cd00b2165cd72f
Sarker Majid <sarkermajid21.bd@gmail.com>
2025-10-13 13:10:33 +0600

    'update'

--- routes/web.php
+++ routes/web.php
@@ -25,35 +25,43 @@ Route::get('admin/login', [AdminController::class, 'AdminLogin'])->name('admin.l
 
 Route::middleware('auth')->group(function () {
     Route::controller(AdminController::class)->group(function () {
+
         Route::get('admin/dashboard', 'AdminDashboard')->name('admin.dashboard');
         Route::get('admin/logout', 'AdminLogout')->name('admin.logout');
         Route::get('admin/profile', 'AdminProfile')->name('admin.profile');
         Route::post('admin/profile/update', 'AdminProfileUpdate')->name('admin.profile.update');
         Route::get('admin/change/password', 'AdminChangePassword')->name('admin.change.password');
         Route::post('admin/update/password', 'AdminUpdatePassword')->name('admin.update.password');
+        
     });
 
     Route::controller(HallController::class)->group(function () {
+
         Route::get('halls', 'Index')->name('hall.index');
         Route::get('hall/add', 'AddHall')->name('hall.add');
         Route::post('hall/store', 'StoreHall')->name('hall.store');
         Route::get('hall/edit/{id}', 'EditHall')->name('hall.edit');
         Route::post('hall/update/{id}', 'UpdateHall')->name('hall.update');
         Route::get('hall/delete/{id}', 'DeleteHall')->name('hall.delete');
+
     });
 
     Route::controller(RoomController::class)->group(function () {
+
         Route::get('rooms', 'Index')->name('room.index');
         Route::get('room/add', 'AddRoom')->name('room.add');
         Route::post('room/store', 'StoreRoom')->name('room.store');
         Route::get('room/edit/{id}', 'EditRoom')->name('room.edit');
         Route::post('room/update', 'UpdateRoom')->name('room.update');
         Route::get('room/delete/{id}', 'DeleteRoom')->name('room.delete');
+
     });
 
     Route::controller(UserController::class)->group(function () {
+
         Route::get('applicant/users', 'ApplicantUsers')->name('applicant.users');
         Route::get('applicant/user/delete', 'ApplicantUserDelete')->name('applicant.user.delete');
+
     });
 });
 
